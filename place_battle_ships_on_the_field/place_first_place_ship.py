f1bda6bbe42cdccd2a9bc74c43a047c6b5b5a43d
Алексей Бобков <bobkovalex.yaundex.ru@yandex.ru>
2024-05-11 18:58:35 +0300

    Однопалубный корабль сделан

--- place_battle_ships_on_the_field/place_first_place_ship.py
+++ place_battle_ships_on_the_field/place_first_place_ship.py
@@ -1,6 +1,6 @@
 # Сделано
 import sys
-sys.path.append('../main_logic')
+sys.path.append('../place_battle_ships_on_the_field')
 from random import randint
 # from main_logic.place_second_place_ship import matrix
 from field_of_game.create_sea_battle_field import matrix
